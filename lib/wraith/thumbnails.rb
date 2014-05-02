efaa2ae3db3c98dc286da00887d58459f600b8ca
Kyle Watson <kw@qial.net>
2014-05-02 13:28:55 -0700

    defwefksjderpderp

--- lib/wraith/thumbnails.rb
+++ lib/wraith/thumbnails.rb
@@ -11,7 +11,9 @@ class Wraith::Thumbnails
     Dir.glob("#{wraith.directory}/*/*.png").each do |filename|
       new_name = filename.gsub(/^#{wraith.directory}/, "#{wraith.directory}/thumbnails")
       puts "Generating thumbnail #{new_name}"
-      Dir.mkdir(File.dirname(new_name))
+      unless Dir.directory?(File.dirname(new_name))
+        Dir.mkdir(File.dirname(new_name))
+      end
       wraith.thumbnail_image(filename, new_name)
     end
     puts 'Generating thumbnails'
