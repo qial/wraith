b66bcac97e551936d8fd66f4211e4ad6e6db1eb9
Kyle Watson <kw@headwire.com>
2014-05-03 00:54:07 +0400

    fixed another issue in thumbnails code

--- lib/wraith/thumbnails.rb
+++ lib/wraith/thumbnails.rb
@@ -11,7 +11,7 @@ class Wraith::Thumbnails
     Dir.glob("#{wraith.directory}/*/*.png").each do |filename|
       new_name = filename.gsub(/^#{wraith.directory}/, "#{wraith.directory}/thumbnails")
       puts "Generating thumbnail #{new_name}"
-      unless Dir.directory?(File.dirname(new_name))
+      unless Dir.exists?(File.dirname(new_name))
         Dir.mkdir(File.dirname(new_name))
       end
       wraith.thumbnail_image(filename, new_name)
@@ -19,4 +19,5 @@ class Wraith::Thumbnails
     puts 'Generating thumbnails'
   end
 end
-
+
+
